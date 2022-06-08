8383490f5da4df44b6a848d9fcecac80189b4650
Elijah Kim <reachelijah@gmail.com>
2022-06-08 16:07:37 -0700

    icons

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: __dirname + '/icons/mac/icon.icns',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
