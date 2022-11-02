895dc52f4e95fdac44717d452d4a635d2ac90dd1
Richard <richard@stromer.org>
2022-11-01 17:06:53 -0700

    extract all parameters from pyinstaller into script (from config file which get overridden anyhow)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Building backend..."
-pyinstaller app.spec --noconfirm
+pyinstaller --noconfirm --upx-dir=/usr/local/bin/ --strip --onefile --add-data "backend/datasets/air_passengers.csv:./datasets" --add-data "backend/datasets/retail_sales.csv:./datasets" --add-data "backend/datasets/wp_log_peyton_manning.csv:./datasets" backend/app.py
 
 echo "Building frontend..."
 (cd client && npm run build)
